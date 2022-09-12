b8cd31390d6a672adfba7b3fa8b0788014a0c00d
wshuts <shutswilliam@gmail.com>
2022-09-12 00:27:36 -0500

    frame and number plane adjustments moved into setup

--- bulleted_list.py
+++ bulleted_list.py
@@ -18,6 +18,11 @@ class BulletedListScriptStyle(MovingCameraScene):
 
     def setup(self):
         MovingCameraScene.setup(self)
+        self.frame.width = FRAME_WIDTH
+        self.frame.move_to(PAN_CAMERA_LEFT * LEFT + PAN_CAMERA_DOWN * DOWN)
+        # self.add(self.number_plane)
+        self.number_plane.width = FRAME_WIDTH
+        self.number_plane.move_to(PAN_CAMERA_LEFT * LEFT + PAN_CAMERA_DOWN * DOWN)
 
     # noinspection PyTypeChecker
     def construct(self):
