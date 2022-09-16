145190bd7a8de9d3a1da71a00858ab29e7ad6452
wshuts <shutswilliam@gmail.com>
2022-09-15 23:43:31 -0500

    removed wait & number_plane

--- bulleted_list_improved.py
+++ bulleted_list_improved.py
@@ -19,7 +19,7 @@ class BulletedListImproved(MovingCameraScene):
         MovingCameraScene.setup(self)
         self.frame.width = FRAME_WIDTH
         self.frame.move_to(PAN_CAMERA_LEFT * LEFT + PAN_CAMERA_DOWN * DOWN)
-        self.add(self.number_plane)
+        # self.add(self.number_plane)
         self.number_plane.width = FRAME_WIDTH
         self.number_plane.move_to(PAN_CAMERA_LEFT * LEFT + PAN_CAMERA_DOWN * DOWN)
 
@@ -49,7 +49,6 @@ class BulletedListImproved(MovingCameraScene):
 
         def animate():
             self.play(FadeIn(self.title, shift=LEFT), GrowFromCenter(self.underline))
-            self.wait(3)
             self.play(Write(self.networks))
             self.play(self.networks.submobjects[0].animate.set_opacity(1))
 
